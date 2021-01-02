a08fb2d9faaacc43fd2aa0dd3df42b44c3d5128b
vaishnavme <vaishnavchandurkar21@outlook.com>
2021-01-02 16:13:38 +0530

    Final Build Done

--- Currency-Converter/js/app.js
+++ Currency-Converter/js/app.js
@@ -1,54 +1,52 @@
-const currencyOne = document.querySelector('#currency-one');
-const currencyTwo = document.querySelector('#currency-two');
+// Selecting html elements
+const select = document.querySelectorAll('select');
+const input = document.querySelectorAll('input');
+
 const showConverted = document.querySelector("#show")
-const convertbtn = document.querySelector("#check");
 
-const amtOne = document.querySelector('#amount-one');
+const API = "https://api.exchangeratesapi.io/latest?base";
+let options = ''
+// Assign elements
+const currencyOne = select[0];
+const currencyTwo = select[1];
 
+const amountInputOne = input[0];
+const amountInputTwo = input[1];
+//
 
-function append(pareantEl, childEl) {
-    return pareantEl.appendChild(childEl);
+function convertHandler(dataRate) {
+    
+    // On input change
+    amountInputOne.addEventListener('input', () => {
+        amountInputTwo.value = (amountInputOne.value * dataRate[currencyTwo.value] / dataRate[currencyOne.value]).toFixed(2);
+    })
+    amountInputTwo.addEventListener('input', () => {
+        amountInputOne.value = (amountInputTwo.value * dataRate[currencyOne.value] / dataRate[currencyTwo.value]).toFixed(2);
+    })
+    // On Selcet change
+    currencyOne.addEventListener('change', ()=> {
+        amountInputTwo.value = (amountInputOne.value * dataRate[currencyTwo.value] / dataRate[currencyOne.value]).toFixed(2);
+    })
+    currencyTwo.addEventListener('change', ()=> {
+        amountInputOne.value = (amountInputTwo.value * dataRate[currencyOne.value] / dataRate[currencyTwo.value]).toFixed(2);
+    })
 }
 
-fetch(`https://api.exchangeratesapi.io/latest?base=`)
-  .then(res => res.json())
-  .then(data => {
-      const rateKey = Object.keys(data.rates)
-      
-      for(let i=0; i<rateKey.length; i++) {
-          let option = document.createElement("option")
-          option.value = rateKey[i];
-          option.text = rateKey[i];
+async function calculate() {
+    const response  = await fetch(API);
+    const data = await response.json();
+    const rates = data.rates;
+    const dataKeys = Object.keys(rates);
+    //console.log(rates)
 
-          let optionClone = option.cloneNode(true)
-          
-          currencyOne.add(option)
-          currencyTwo.add(optionClone);
+    dataKeys.map(items => {
+        return options += `<option value=${items}>${items}</option>`
+    })
+    
+    for(let i = 0; i<select.length; i++) {
+        select[i].innerHTML = options;
     }
-}); 
-
-
-function calculate() {
-    let currOne = currencyOne.value;
-    let currTwo = currencyTwo.value;
-    if(currOne == "" || currTwo == "") {
-        showConverted.value = "Please Select Currency";
-        return
-    }
-    fetch(`https://api.exchangeratesapi.io/latest?base=${currOne}`)
-    .then(res => res.json())
-    .then(data => {
-        const rate = data.rates[currTwo];
-        showConverted.value = (amtOne.value * rate).toFixed(2);
-    });
+    convertHandler(rates)
 }
-swap.addEventListener('click', () => {
-    const temp = currencyOne.value;
-    currencyOne.value = currencyTwo.value;
-    currencyTwo.value = temp;
-    calculate();
-});
-currencyOne.addEventListener('change', calculate);
-amtOne.addEventListener('input', calculate);
-currencyTwo.addEventListener('change', calculate);
-convertbtn.addEventListener("click", calculate)
+
+calculate();
