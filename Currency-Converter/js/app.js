9f80c461b334371cb829e71ab88d83b41e222320
vaishnavme <vaishnavchandurkar21@outlook.com>
2021-01-02 09:34:41 +0530

    Working but not final

--- Currency-Converter/js/app.js
+++ Currency-Converter/js/app.js
@@ -1,41 +1,45 @@
+const currencyOne = document.querySelector('#currency-one');
+const currencyTwo = document.querySelector('#currency-two');
 
-const currencyOne = document.getElementById('currency-one');
-const currencyTwo = document.getElementById('currency-two');
-const amtOne = document.getElementById('amount-one');
-const amtTwo = document.getElementById('amount-two');
+const amtOne = document.querySelector('#amount-one');
+const amtTwo = document.querySelector('#amount-two');
 
-const convert = document.getElementById('swap');
-const rate = document.getElementById('rate');
+function append(pareantEl, childEl) {
+    return pareantEl.appendChild(childEl);
+}
 
+fetch(`https://api.exchangeratesapi.io/latest?base=`)
+  .then(res => res.json())
+  .then(data => {
+      const rateKey = Object.keys(data.rates)
+      
+      for(let i=0; i<rateKey.length; i++) {
+          let option = document.createElement("option")
+          option.value = rateKey[i];
+          option.text = rateKey[i];
+
+          let optionClone = option.cloneNode(true)
+          
+          currencyOne.add(option)
+          currencyTwo.add(optionClone);
+    }
+}); 
 
 function calculate() {
-    const currOne = currencyOne.value;
-    const currTwo = currencyTwo.value;
-
+    let currOne = currencyOne.value;
+    let currTwo = currencyTwo.value;
     fetch(`https://api.exchangeratesapi.io/latest?base=${currOne}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        const rateKey = Object.keys(data.rates)
-        console.log(rateKey)
         const rate = data.rates[currTwo];
-        rate.innerText = `1 ${currOne} = ${rate} ${currTwo}`;
         amtTwo.value = (amtOne.value * rate).toFixed(2);
     });
 }
 
-// Event Listener
 
 currencyOne.addEventListener('change', calculate);
 amtOne.addEventListener('input', calculate);
 currencyTwo.addEventListener('change', calculate);
 amtTwo.addEventListener('input', calculate);
 
-swap.addEventListener('click', () => {
-    const temp = currencyOne.value;
-    currencyOne.value = currencyTwo.value;
-    currencyTwo.value = temp;
-    calculate();
-  });
-  
-calculate();
+
